17b3987652767cb65a5fc6ce551b912603855098
20231448-png <20231448@eaut.edu.vn>
2026-03-27 21:24:14 +0700

    Add files via upload

---
+++ 27-3/2_3_Bai1.py
@@ -0,0 +1,4 @@
+_a = input("Nhap so a: ")
+_b = input("Nhap so b: ")
+_c = int(_a) + int(_b)
+print("Tong cua a va b la: ", _c)
---
+++ 27-3/2_3_Bai4.py
@@ -0,0 +1,4 @@
+_x = input("Nhap mot chuoi: ")
+_y = input("Nhap mot chuoi khác: ")
+_z = input("Nhap mot chuoi nữa: ")
+print(_x + " " + _y + " " + _z)
